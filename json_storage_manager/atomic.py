aab0fce7014953834e7afeb529ae473fe3cabef2
Ahmed Hefnawi <me@ahmedhefnawi.com>
2018-11-13 13:19:57 +0200

    jsm: handle nested objects for orders api - fix update_item()

--- json_storage_manager/atomic.py
+++ json_storage_manager/atomic.py
@@ -74,8 +74,15 @@ def update_item(filename, item, uuid):
             # load the JSON data into memory
             products_data = json.load(products_file)
         # apply modifications to the JSON data wrt UUID
-        [products_data[i].update(item) for (
-            i, j) in enumerate(products_data) if j["uuid"] == str(uuid)]
+        # TODO: handle this in a neat way
+        if 'products' in products_data[-1]:
+            # handle orders object
+            [products_data[i]["products"].update(item) for (
+                i, j) in enumerate(products_data) if j["uuid"] == str(uuid)]
+        else:
+            # handle products object
+            [products_data[i].update(item) for (i, j) in enumerate(
+                products_data) if j["uuid"] == str(uuid)]
         # save the modified JSON data into the temp file
         json.dump(products_data, temp_file)
         return True
